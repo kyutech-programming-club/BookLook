84c6a14f2b0c407ff2b1864a8081a4dabb9f86ca
ryutaro-kimura <sasuker2264@gmail.com>
2020-10-17 01:14:34 +0900

    create auth function and sql

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
-from api import test_bp
+from api import book_regist_bp
 
 app = Flask(__name__, static_folder='../frontend/dist/static', template_folder='../frontend/dist')
 
-app.register_blueprint(test_bp)
+app.register_blueprint(book_regist_bp)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
