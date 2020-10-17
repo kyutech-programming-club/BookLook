798a57114f7bf0c4b96a1e34597f39366f9c518c
o-xian999 <onoyama1999910@gmail.com>
2020-10-17 15:06:43 +0900

    Send unsename api

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -5,12 +5,38 @@
     </v-card-title>
     <v-card-text>
       <v-form>
-        <v-text-field label="ユーザ名" />
-        <v-text-field label="パスワード" />
+        <v-text-field
+          v-model="username"
+          label="ユーザ名" />
+        <v-text-field
+          v-model="pass"
+          label="パスワード" />
         <v-card-actions>
-      <v-btn>登録</v-btn>
-  </v-card-actions>
+      <v-btn @click="sendRegister">登録</v-btn>
+    </v-card-actions>
       </v-form>
     </v-card-text>
   </v-card>
 </template>
+
+<script>
+export default {
+  name: 'Register'
+  data(){
+    return {
+      username: ""
+    }
+  },
+  methods: {
+    async sendRegister(){
+      const res = await this.$axios.post(
+        '/auth/register'
+        {
+          username: this.username,
+        }
+        this.test = res.data.body
+      )
+    }
+  }
+}
+</script>
