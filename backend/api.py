be21f91fd31d75616f4f1c8a706d64bbaf3ba205
o-xian999 <onoyama1999910@gmail.com>
2020-10-18 14:54:35 +0900

    Rdit main.py

--- backend/api.py
+++ backend/api.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request, session
 from flask_restful import Api, Resource
 import json
-from backend.models import register_book
+from backend.models import register_book, get_all
 
 test_bp = Blueprint('test', __name__, url_prefix='/api/test')
 class Test(Resource):
